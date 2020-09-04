701dc1851dba455ed364044c12b4362d5bb47514
Prachal80 <prachaljpatel@gmail.com>
2020-09-03 23:07:16 -0700

    Initial Commit

---
+++ Flixy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixy
+//
+//  Created by Prachal Patel on 9/3/20.
+//  Copyright © 2020 Prachal Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
